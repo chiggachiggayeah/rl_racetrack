eb4e05537fef274bf9e0702c3fef949b71b14080
chiggachiggayeah <chiggachiggayeah@users.noreply.github.com>
2018-01-06 19:07:39 -0500

    first commit

---
+++ montecarlo.js
@@ -0,0 +1,228 @@
+// will implement both off-policy and on-policy methods
+// for the Racetrack problem presented in Introduction To Reinforcement Learning (Sutton))
+// the actions are the increments to the various components of velocity (9 total)
+// the velocity here determines next states
+// have to make sure that there's only one startSqr or start at a random start square
+// might have to invert velocity since up one is x - 1 (0,0 in the top left corner of the canvas)
+
+let actions = [[0,0],[1,1],[-1,-1],[1,0],[0,1],[1,-1],[-1,1],[-1,0],[0,-1]]
+
+let trackSqrs = Object.values(squares)
+let startSqrs = Object.values(start)
+let finishSqrs = Object.values(finish)
+
+let states = startSqrs.concat(trackSqrs).concat(finishSqrs)
+
+let serializeState = function(s) {
+    return s.x + "&" + s.y
+}
+
+let Car = function(pos) {
+    let velocity = {
+        x: 0,
+        y: 0
+    }
+    let position = {
+        x: pos.x,
+        y: pos.y
+    }
+    let c = {
+        getVelocity: function() {
+            return velocity
+        },
+        getPosition: function() {
+            return position
+        },
+        getReward: function() {
+            // have we tried to head off the track?
+            // if not then rewards(s, a).push(-1)
+            // else rewards(s,a).push(-5)
+        },
+        updateVelocity: function(unit) {
+            velocity.x += unit[0]
+            velocity.y += unit[1]
+
+            if(velocity.y > 0) velocity.y -= 1
+            if(velocity.x < 0) velocity.x += 1 // only right turns
+
+            // clamp both components to be less than or equal to five
+            if(velocity.y < -5) velocity.y += 1
+            if(velocity.x > 5) velocity.x -= 1
+        },
+        step: function() {
+            // this function will consider the present velocity
+            // but only update to valid positions (positions on the track)
+            
+            // let p = this.getPosition()
+            // let v = c.getVelocity()
+            
+            let hypoPos = {
+                x: position.x + velocity.x,
+                y: position.y + velocity.y   
+            
+            }
+            if(!this.onTrack(hypoPos)){
+                // move in one of three directions
+                let hp1 = {
+                    x: p.x + v.x,
+                    y: p.y
+                }
+                let hp2 = {
+                    x: p.x,
+                    y: p.y + v.y
+                }
+                if(this.onTrack(hp1)){
+                    position.x += velocity.x
+                } else if(this.onTrack(hp2)){
+                    position.y += velocity.y 
+                } else {
+                    // try each of the three directions inc by 1
+                    let h1 = position
+                    let h2 = position
+                    let h3 = position
+
+                    h1.x += 1
+                    h2.x -= 1
+                    h3.y += 1
+
+                    if(this.onTrack(h1)) {
+                        position = h1
+                    } else if(this.onTrack(h2)) {
+                        position = h2
+                    } else if(this.onTrack(h3)) {
+                        position = h3
+                    }
+                }
+            } else {
+                position = hypoPos
+            }
+            
+            return position 
+        },
+        atFinish: function() {
+            let p = this.getPosition()
+            
+            // return whether or not we've arrived at the finish 
+            let result = finishSqrs.reduce((acc, cv) => {
+                return acc || (cv.x == p.x && cv.y == p.y)
+            })
+            return result
+        },
+        onTrack: function(pos) {
+            return squares[serializeState(pos)] != undefined 
+        }
+    }
+
+    return c
+}
+
+let Q = function(){
+    // let trackSqrs = Object.values(squares)
+    // let startSqrs = Object.values(start)
+    // let finishSqrs = Object.values(finish)
+
+    // let states = startSqrs.concat(trackSqrs).concat(finishSqrs)
+    // let actions = [-1, 0, 1] // various increments to velocity
+
+    let serializeState = function(s) {
+        return s.x + "&" + s.y
+    }
+
+    let initializeQ = function(){
+        let q = {}
+        for(i in states) {
+            q[serializeState(states[i])] = []            
+            for(ind in actions) q[serializeState(states[i])].push(0)
+        }
+        return q
+    }
+
+    return initializeQ()
+}
+
+let Returns = function() {
+    // let trackSqrs = Object.values(squares)
+    // let startSqrs = Object.values(start)
+    // let finishSqrs = Object.values(finish)
+
+    // let states = startSqrs.concat(trackSqrs).concat(finishSqrs)
+
+    let serializeState = function(s) {
+        return s.x + "&" + s.y
+    }
+
+    let initializeReturns = function() {
+        let r = {}
+        for(i in states) {
+            r[serializeState(states[i])] = []
+            for(ind in actions) r[serializeState(states[i])].push([])   
+        }
+        return r
+    }
+
+    return initializeReturns()
+}
+
+// for on-policy control, arbitrarily init an e-soft (p(s,a) > 0 for all (s,a))
+let Policy = function() {
+    
+    let serializeState = function(s) {
+        return s.x + "&" + s.y
+    }
+
+    let initializePolicy = function() {
+        let p = {}
+        for(i in states) {
+            p[serializeState(states[i])] = []
+            for(ind in actions) p[serializeState(states[i])].push(1/actions.length)
+        }
+        return p
+    }
+}
+
+let getMaxAction = function(s) {
+    let m = s.reduce((acc, cv) => Math.max(acc, cv))
+    return m
+}
+
+let generateEpisode = function(policy) {
+    // generate a sequence of states and actions from the policy
+    let pos = startSqrs
+    let c = Car(pos)
+    let pairs = []
+    while(true) {
+        let action = getMaxAction(p[serializeState(pos)])
+        // see the result of hypothetically applying the action
+        // if it would take you off the track, then don't do it (do inc so that you stay on the track) 
+        // else then take the action
+        // let c.updateVelocity(action)
+
+        pairs.push([pos, action])
+
+        c.updateVelocity(actions[action])
+        pos = c.step()         
+
+        // check if we're done
+        if(c.atFinish()) break
+    }
+
+    return pairs
+}
+
+let onPolicy = function() {
+    // init policy
+    // init action values
+    // init returns
+    // let returnsForEpisode = {} // a dictionary that maps to arrays(actions) 
+    // you have to keep track of what (state, action) you've seen before
+
+    // repeat forever (?)
+    // generate episode using policy
+    // for all (s,a) in episode
+        //  r <- first occurence reward for (s,a)
+        //  R(s,a).append(r)
+        //  Q(s,a) = mean(R(s,a))
+    // foreach s in the episode, policy improve with e-greedy
+    
+}
+
---
+++ track.js
@@ -0,0 +1,237 @@
+// TODO
+// [] allow overwriting so if I want to make a track square a finish or start square I can
+
+
+let grid = []
+let cWidth = 50
+let cHeight = 50
+let cellW = 10
+let cellH = 10
+let squares = {}
+let start = {}
+let finish = {}
+let squaresArr = {} //<- change squares to a nested array [x][y]
+
+let state = (function(){
+    let drawingStart = false
+    let drawingFinish = false
+    let drawingTrack = true
+    let training = false
+
+    let s = {
+        drawTrack: function() {
+            drawingStart = false
+            drawingFinish = false
+            drawingTrack = true
+            training = false
+        },
+
+        drawFinish: function() {
+            drawingStart = false
+            drawingFinish = true
+            drawingTrack = false
+            training = false
+
+        },
+
+        drawStart: function() {
+            drawingStart = true
+            drawingFinish = false
+            drawingTrack = false
+            training = false
+
+        },
+
+        startTraining: function() {
+            drawingStart = false
+            drawingFinish = false
+            drawingTrack = false
+            training = true
+        },
+
+        isTraining: () => training,
+        isDrawingTrack: () => drawingTrack,
+        isDrawingStart: () => drawingStart,
+        isDrawingFinish: () => drawingFinish
+    }
+
+    return s
+})()
+
+function setup(){
+    createCanvas(cWidth * cellW + cellW, cHeight * cellH + cellH)   
+}
+
+function buildGrid(){
+    for(var x = 0; x < cWidth; x++) {
+        for(var y = 0; y < cHeight; y++){
+            rect(x * cellW, y * cellH, cellW, cellH)
+        }
+    }
+}
+
+
+function getSquare(mouseX, mouseY) {
+    let color = state.isDrawingTrack() ? 'rgb(0, 0, 0)' : (state.isDrawingFinish() ? 'rgb(255, 0, 0)' : 'rgb(10, 255, 10)')
+    return {
+        x: Math.floor(mouseX / 10) * cellW,
+        y: Math.floor(mouseY / 10) * cellH,
+        color: color 
+    }
+}
+
+function drawSquares() {
+       
+    if(Object.keys(squares).length > 0) {
+        for(k in squares){
+            // console.log("Key is: ", k)
+            let pos = squares[k]
+            fill(pos.color)
+            rect(pos.x, pos.y, cellW, cellH) 
+        }
+    }
+    if(Object.keys(start).length > 0) {
+        for(k in start){
+            // console.log("Key is: ", k)
+            let pos = start[k]
+            fill(pos.color)
+            rect(pos.x, pos.y, cellW, cellH) 
+        }
+    }
+    if(Object.keys(finish).length > 0) {
+        for(k in finish){
+            // console.log("Key is: ", k)
+            let pos = finish[k]
+            fill(pos.color)
+            rect(pos.x, pos.y, cellW, cellH) 
+        }
+    }
+
+     
+}
+
+function addToTrack() {
+    if(mouseIsPressed && !state.isTraining()){
+        if(mouseX <= cWidth * cellW && mouseX >= 0 & mouseY >= 0 & mouseY <= cHeight * cellH){
+            // console.log("keys in squares: ", Object.keys(squares).toString())
+            let pos = getSquare(mouseX, mouseY)
+            // console.log("Mouse x: ", pos.x, " Mouse y: ", pos.y) 
+            let newKey = pos.x.toString() + "&" + pos.y.toString()
+            // console.log("new key is: ", newKey)
+            if(squaresArr[pos.x] == undefined) squaresArr[pos.x] = []
+            // I only want to add this to my squaresArr (for filling) if it's not been added)
+            if(squares[newKey] == undefined) squaresArr[pos.x].push(pos)
+
+            
+            if(state.isDrawingTrack()) squares[newKey] = pos   
+            if(state.isDrawingFinish()) finish[newKey] = pos
+            if(state.isDrawingStart()) start[newKey] = pos
+
+                        // console.log("newly stored value: ", squares[newKey].x)
+        }
+
+    }    
+}
+
+function fillTrack() {
+    if(state.isDrawingTrack()){
+        console.log("number of squares before fill: ", Object.keys(squares).length)
+        let xArray = Object.values(squares).map((v) => v.x)
+        let yArray = Object.values(squares).map((v) => v.y)
+
+        // console.log("X's: ", xArray.toString())
+        // console.log("Y's: ", yArray.toString())
+
+        let xMin = xArray.reduce((acc, cv) => {
+            return Math.min(acc, cv)
+        })      
+        let xMax = xArray.reduce((acc, cv) => {
+            return Math.max(acc, cv)
+        })
+        let yMin = yArray.reduce((acc, cv) => {
+            return Math.min(acc, cv)
+        }) 
+        let yMax = yArray.reduce((acc, cv) => {
+            return Math.max(acc, cv)
+        })
+        
+        
+
+        console.log("xMax: ", xMax, " xMin: ", xMin)
+        console.log("yMax: ", yMax, " yMin: ", yMin)
+        
+        let newAdded = [] 
+        
+        for(let i = xMin; i < xMax; i+=cellW) {
+            // get the bounding y's of the shape at the x and fill along the x between the y's
+            let localYs = squaresArr[i].map(p => p.y) 
+            console.log("Local y's array: ", squaresArr[i].toString())
+            let localYMax = localYs.reduce((acc, cv) => Math.max(acc, cv))
+            let localYMin = localYs.reduce((acc, cv) => Math.min(acc, cv))
+            console.log("Local y min: ", localYMin, " local y max: ", localYMax)
+            for(let j = localYMin + cellH; j < localYMax; j+=cellH){ 
+                let pos = {
+                    x: i,
+                    y: j,
+                    color: 'rgb(0,0,0)'
+                } 
+                let newKey = pos.x + "&" + pos.y
+                squares[newKey] = pos
+            }
+        }
+
+        // for(let x = 0; x < cWidth; x++) {
+        //     for(let y = 0; y < cHeight; y++) {
+        //         if(x * cellW > xMin && x * cellW < xMax) {
+        //             if(y * cellH > yMin && y * cellH < yMax) {
+        //                 let pos = {
+        //                     x: x * cellW,
+        //                     y: y * cellH,
+        //                     color: 'rgb(0,0,0)'
+        //                 }
+        //                 let newKey = pos.x.toString() + "&" + pos.y.toString()
+        //                 squares[newKey] = pos
+        //                 newAdded.push(pos)
+        //             }
+        //         }
+        //     }
+        // } 
+        console.log("number of squares after fill: ", Object.keys(squares).length)
+        console.log("number of new squares: ", newAdded.length)
+        for(let i = 0; i < newAdded.length; i++) {
+            console.log("New x: ", newAdded[i].x, " New y: ", newAdded[i].y)
+        }
+    } 
+}
+
+let getTrackStates = function() {
+}
+
+let printOutStates = function printOutStates() {
+    let obj = {
+        start: [],
+        finish: [],
+        track: []
+    }
+    // obj.start = Object.values(start).map((x) => JSON.stringify(x))
+    // obj.finish = Object.values(finish).map((x) => JSON.stringify(x))
+    // obj.track = Object.values(squares).map((x) => JSON.stringify(x))
+   
+    obj.start = Object.values(start)
+    obj.finish = Object.values(finish)
+    obj.track = Object.values(squares)
+
+    let stringified = JSON.stringify(obj)
+    console.log(stringified)
+    console.log(JSON.parse(stringified))
+    return stringified
+}
+
+function draw() {
+    addToTrack()
+    fill(255)
+    stroke(0) 
+    // rect(cellW, cellH, cellW, cellH)
+    buildGrid()
+    drawSquares()
+}
